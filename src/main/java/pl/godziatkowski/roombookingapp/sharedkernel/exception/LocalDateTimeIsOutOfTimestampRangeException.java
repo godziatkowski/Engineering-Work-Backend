d6d284221420e888aeb0ad2fe1880c9878bf252c
Jan Godziatkowski <ph.godzio@gmail.com>
2016-01-23 03:00:54 +0100

    Rename project package name
    *remove underscores from package name

---
+++ src/main/java/pl/godziatkowski/roombookingapp/sharedkernel/exception/LocalDateTimeIsOutOfTimestampRangeException.java
@@ -0,0 +1,16 @@
+package pl.godziatkowski.roombookingapp.sharedkernel.exception;
+
+public class LocalDateTimeIsOutOfTimestampRangeException
+   extends RuntimeException {
+
+   private static final long serialVersionUID = 8288277651777709068L;
+
+   public LocalDateTimeIsOutOfTimestampRangeException() {
+      super();
+   }
+
+   public LocalDateTimeIsOutOfTimestampRangeException(String message) {
+      super(message);
+   }
+
+}
