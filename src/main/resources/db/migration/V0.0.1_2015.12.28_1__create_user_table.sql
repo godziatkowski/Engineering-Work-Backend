bbd8b233cbdf4c13f78103e84ae3cd8077c312d6
Jan Godziatkowski <ph.godzio@gmail.com>
2016-01-23 03:09:33 +0100

    Create tables to store data about users and other tables required by OAuth2

---
+++ src/main/resources/db/migration/V0.0.1_2015.12.28_1__create_user_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE user (
+	id BIGINT (20) NOT NULL AUTO_INCREMENT,
+	login VARCHAR(20) NOT NULL,
+	password VARCHAR(60) DEFAULT NULL,
+	first_name VARCHAR(25) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE KEY login (login),
+	UNIQUE KEY idx_user_login(login)
+	);
+
+CREATE TABLE user_user_roles (
+    user_id BIGINT NOT NULL,
+    user_roles VARCHAR(25) NOT NULL
+    );
+
+CREATE TABLE user_watched_rooms (
+	user_id BIGINT NOT NULL,
+	watched_rooms BIGINT NOT NULL
+	);
