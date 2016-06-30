87ce2474c03a4cfd90c6684dd7b47c6a5486fd60
Jan Godziatkowski <ph.godzio@gmail.com>
2016-06-30 22:41:07 +0200

    emails

---
+++ src/main/java/pl/godziatkowski/roombookingapp/config/MailConfigurator.java
@@ -0,0 +1,22 @@
+package pl.godziatkowski.roombookingapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class MailConfigurator {
+
+    @Bean
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+        emailTemplateResolver.setPrefix("mails/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode("HTML5");
+        emailTemplateResolver.setCharacterEncoding("UTF-8");
+        emailTemplateResolver.setOrder(1);
+        emailTemplateResolver.setCacheable(false);
+
+        return emailTemplateResolver;
+    }
+}
