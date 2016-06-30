d743ad076b07b4230ec844f04b42e14a6aa1979a
Jan Godziatkowski <ph.godzio@gmail.com>
2016-06-30 22:30:44 +0200

    Add possibility of sending emails from application

---
+++ src/main/java/pl/godziatkowski/roombookingapp/sharedkernel/exception/EmailSendingException.java
@@ -0,0 +1,8 @@
+package pl.godziatkowski.roombookingapp.sharedkernel.exception;
+
+public class EmailSendingException
+    extends Exception {
+
+    private static final long serialVersionUID = 6113261373565075601L;
+
+}
